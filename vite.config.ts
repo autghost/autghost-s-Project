b795eac5f4938eaf3c247486c5125c279d63b51a
autghost <34340759+autghost@users.noreply.github.com>
2026-03-03 15:26:18 +0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.ico', '180x180.png'],
       manifest: {
         name: '听写助手',
         short_name: '听写助手',
@@ -20,20 +20,20 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: '192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: '512x512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'apple-touch-icon.png',
+            src: '180x180.png',
             sizes: '180x180',
             type: 'image/png',
-            purpose: 'apple touch icon'
+            purpose: 'any'
           }
         ]
       },
